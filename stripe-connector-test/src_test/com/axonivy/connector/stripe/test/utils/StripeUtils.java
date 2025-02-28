4f720a9ec263ed2fdac2b3583a40fd90cb776e8b
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-02-28 17:52:06 +0700

    update code

---
+++ stripe-connector-test/src_test/com/axonivy/connector/stripe/test/utils/StripeUtils.java
@@ -0,0 +1,13 @@
+package com.axonivy.connector.stripe.test.utils;
+
+import ch.ivyteam.ivy.environment.AppFixture;
+
+public class StripeUtils {
+
+  public static void setUpConfigForApiTest(AppFixture fixture) {
+    String secretKey = System.getProperty("secretKey");
+    String publishableKey = System.getProperty("publishableKey");
+    fixture.var("stripe.auth.secretKey", secretKey);
+    fixture.var("stripe.auth.publishableKey", publishableKey);
+  }
+}
