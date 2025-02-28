1510397cc3a188a5566d634fff7806f973157ad6
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-02-28 11:52:54 +0700

    remove static

--- stripe-connector-test/src_test/com/axonivy/connector/stripe/test/PaymentServiceTest.java
+++ stripe-connector-test/src_test/com/axonivy/connector/stripe/test/PaymentServiceTest.java
@@ -29,7 +29,7 @@ public class PaymentServiceTest {
       "/stripe-connector-test/1946E968E7BAB355/logInUser.ivp?username=Developer&password=Developer";
 
   @BeforeEach
-  public static void setup(AppFixture appFixture) {
+  void setup(AppFixture appFixture) {
     StripeUtils.setUpConfigForApiTest(appFixture);
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless"); // Run in headless mode (no GUI)
