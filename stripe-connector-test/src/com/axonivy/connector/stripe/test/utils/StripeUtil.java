92a6d0624e13ad9286d93036832b1e52d1f18429
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-03-14 10:27:54 +0700

    handle feedback

--- stripe-connector-test/src/com/axonivy/connector/stripe/test/utils/StripeUtil.java
+++ stripe-connector-test/src/com/axonivy/connector/stripe/test/utils/StripeUtil.java
@@ -3,12 +3,10 @@ package com.axonivy.connector.stripe.test.utils;
 public class StripeUtil {
 
 	public static String getSecret() {
-//		return System.getProperty("secretKey");
-		return "sk_test_51Qe9v3LaeAomYD3LDQoSSPIlT5XaWCDP19UYwOnUw7ODspcVu7C2WzAQq9E1PiEXn6GagMNMaEd1My2E4yNq3MGA005gofIbbz";
+		return System.getProperty("secretKey");
 	}
 	
 	public static String getPublic() {
-//		return System.getProperty("publishableKey");
-		return "pk_test_51Qe9v3LaeAomYD3LNDn8V1WzZSn6HSx8AHVeKxq8ZGee5Q8ONKDGsrL6hXLFj33gvzl080xMcdhQXMS4fDzipJeX00Fe0BdnpK";
+		return System.getProperty("publishableKey");
 	}
 }
