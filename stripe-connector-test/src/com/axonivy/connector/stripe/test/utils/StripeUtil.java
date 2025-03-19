ff99d4971613550746d3611fc5af63dabc383271
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-03-19 18:10:46 +0700

    test

--- stripe-connector-test/src/com/axonivy/connector/stripe/test/utils/StripeUtil.java
+++ stripe-connector-test/src/com/axonivy/connector/stripe/test/utils/StripeUtil.java
@@ -3,10 +3,14 @@ package com.axonivy.connector.stripe.test.utils;
 public class StripeUtil {
 
 	public static String getSecret() {
-		return System.getProperty("secretKey");
+		String a = System.getProperty("secretKey");
+		System.out.println("#getSecret----- " + a);
+		return "sk_test_51Qe9v3LaeAomYD3LDQoSSPIlT5XaWCDP19UYwOnUw7ODspcVu7C2WzAQq9E1PiEXn6GagMNMaEd1My2E4yNq3MGA005gofIbbz";
 	}
 	
 	public static String getPublic() {
-		return System.getProperty("publishableKey");
+		String b = System.getProperty("publishableKey");
+		System.out.println("#getPublic----- " + b);
+		return "pk_test_51Qe9v3LaeAomYD3LNDn8V1WzZSn6HSx8AHVeKxq8ZGee5Q8ONKDGsrL6hXLFj33gvzl080xMcdhQXMS4fDzipJeX00Fe0BdnpK";
 	}
 }
