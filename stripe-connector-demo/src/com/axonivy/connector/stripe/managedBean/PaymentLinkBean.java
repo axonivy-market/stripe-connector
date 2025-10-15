841058c3fad36c3d79135d561f6ed4ffc5f9e4aa
Thai Phan <thai.phanvan@axonactive.com>
2025-10-15 11:56:50 +0700

    MARP-3051 Enhance payment link operators - Add demos

--- stripe-connector-demo/src/com/axonivy/connector/stripe/managedBean/PaymentLinkBean.java
+++ stripe-connector-demo/src/com/axonivy/connector/stripe/managedBean/PaymentLinkBean.java
@@ -1,6 +1,7 @@
 package com.axonivy.connector.stripe.managedBean;
 
 import com.axonivy.connector.stripe.service.PaymentService;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.stripe.exception.StripeException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -12,10 +13,36 @@ public class PaymentLinkBean {
   private long quantity;
   private String result;
   private boolean disableForm;
+  private String paymentLinkId;
+  private boolean active;
+  private String createResult;
+  private String retrieveResult;
+  private String retrieveLineItemsResult;
+  private String setActiveResult;
 
   public void onSendRequest() throws StripeException {
     result = PaymentService.getInstance().getPaymentLink(priceId, quantity);
     this.disableForm = true;
+  }
+
+  public void onCreatePaymentLink() throws StripeException {
+    JsonNode jsonResult = PaymentService.getInstance().createPaymentLink(priceId, quantity);
+    createResult = jsonResult != null ? jsonResult.toPrettyString() : "No result";
+  }
+
+  public void onRetrievePaymentLink() throws StripeException {
+    JsonNode jsonResult = PaymentService.getInstance().retrievePaymentLink(paymentLinkId);
+    retrieveResult = jsonResult != null ? jsonResult.toPrettyString() : "No result";
+  }
+
+  public void onRetrievePaymentLinkLineItems() throws StripeException {
+    JsonNode jsonResult = PaymentService.getInstance().retrievePaymentLinkLineItems(paymentLinkId);
+    retrieveLineItemsResult = jsonResult != null ? jsonResult.toPrettyString() : "No result";
+  }
+
+  public void onSetPaymentLinkActive() throws StripeException {
+    JsonNode jsonResult = PaymentService.getInstance().setPaymentLinkActive(paymentLinkId, active);
+    setActiveResult = jsonResult != null ? jsonResult.toPrettyString() : "No result";
   }
 
   public String getPriceId() {
@@ -48,6 +75,55 @@ public class PaymentLinkBean {
 
   public void setDisableForm(boolean disableForm) {
     this.disableForm = disableForm;
+  }
+
+  // Getters and setters for new fields
+  public String getPaymentLinkId() {
+    return paymentLinkId;
+  }
+
+  public void setPaymentLinkId(String paymentLinkId) {
+    this.paymentLinkId = paymentLinkId;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public String getCreateResult() {
+    return createResult;
+  }
+
+  public void setCreateResult(String createResult) {
+    this.createResult = createResult;
+  }
+
+  public String getRetrieveResult() {
+    return retrieveResult;
+  }
+
+  public void setRetrieveResult(String retrieveResult) {
+    this.retrieveResult = retrieveResult;
+  }
+
+  public String getRetrieveLineItemsResult() {
+    return retrieveLineItemsResult;
+  }
+
+  public void setRetrieveLineItemsResult(String retrieveLineItemsResult) {
+    this.retrieveLineItemsResult = retrieveLineItemsResult;
+  }
+
+  public String getSetActiveResult() {
+    return setActiveResult;
+  }
+
+  public void setSetActiveResult(String setActiveResult) {
+    this.setActiveResult = setActiveResult;
   }
 
 }
