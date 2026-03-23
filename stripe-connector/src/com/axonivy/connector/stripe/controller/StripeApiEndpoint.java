68de3a9401c16ce02e31332bf0863908741e6f4b
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2026-03-23 16:34:47 +0700

    test

--- stripe-connector/src/com/axonivy/connector/stripe/controller/StripeApiEndpoint.java
+++ stripe-connector/src/com/axonivy/connector/stripe/controller/StripeApiEndpoint.java
@@ -1,9 +1,16 @@
 package com.axonivy.connector.stripe.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import com.axonivy.connector.stripe.service.PaymentService;
 import com.stripe.exception.StripeException;
@@ -19,4 +26,13 @@ public class StripeApiEndpoint {
     String clientSecret = PaymentService.getInstance().getClientSecret(priceId, quantity);
     return Response.ok("{\"clientSecret\":\"" + clientSecret + "\"}").build();
   }
+  
+  @GET
+  @Path("/read")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String readFile(@QueryParam("name") String fileName) throws IOException {
+      File file = new File("/app/data/" + fileName);
+      return new String(Files.readAllBytes(file.toPath()));
+  }
+  
 }
